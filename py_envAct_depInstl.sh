bfbcb02de3d2cbac928dd71ffd46114525f420ff
prgrmz07 <prgrmz07@163.com>
2024-06-20 17:55:03 +0800

    删除脚本中的开发语句

--- py_envAct_depInstl.sh
+++ py_envAct_depInstl.sh
@@ -5,7 +5,9 @@
 cd /fridaAnlzAp/frida_js/
 
 # 激活py环境
-set +x ; source /app/Miniconda3-py310_22.11.1-1/bin/activate ; set -x
+# set +x ; 
+source /app/Miniconda3-py310_22.11.1-1/bin/activate ; 
+# set -x
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple > /dev/null
 # py依赖安装
 pip install -r requirements.txt > /dev/null
