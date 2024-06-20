40fb07e5dbbb633e44cf5469d720028e883d57b0
prgrmz07 <prgrmz07@163.com>
2024-06-20 12:09:23 +0800

    自制拆分ts ： 拆出_nativeFn__TL_TmPnt__update [2]， 符合名字规范 模块局变量 mg_

--- _nativeFn__TL_TmPnt__update.ts
+++ _nativeFn__TL_TmPnt__update.ts
@@ -3,6 +3,7 @@
 // [描述] 描述 本地函数 TL_TmPnt__update
 // [业务描述]  
 //    函数进入时, 调用本地函数 'clang-var运行时基础 中的 TL_TmPnt__update(tmPntVal)', 用以表达 此线程的此次函数调用的 _vdLs 和 时刻点 tmPntVal 一 一 对 应
+// [术语] : mg_x 见 readme.md
 
 /**  函数描述
 
@@ -15,16 +16,16 @@
   */
 
 // 持有本地函数 'clang-var运行时基础 中的 TL_TmPnt__update(tmPntVal)'
-let g_nativeFn__clgVarRt__TL_TmPnt__update:NativeFunction<void,[ThreadId,MG_TmPntVal]>  |null;  
+let mg_nativeFn__clgVarRt__TL_TmPnt__update:NativeFunction<void,[ThreadId,MG_TmPntVal]>  |null;  
 // ThreadId == number , MG_TmPntVal == number 
 
 // 调用本地函数 'clang-var运行时基础 中的 TL_TmPnt__update(tmPntVal)'
 function call_nativeFn__TL_TmPnt__update(curThreadId:ThreadId,tmPntVal:MG_TmPntVal){
 
   //调用 clang-var运行时基础 中函数 TL_TmPnt__update(tmPntVal)
-  if(g_nativeFn__clgVarRt__TL_TmPnt__update){
+  if(mg_nativeFn__clgVarRt__TL_TmPnt__update){
     // const ret= //无返回值
-    g_nativeFn__clgVarRt__TL_TmPnt__update(curThreadId,tmPntVal);
+    mg_nativeFn__clgVarRt__TL_TmPnt__update(curThreadId,tmPntVal);
   }
 
 }
@@ -33,5 +34,5 @@ function call_nativeFn__TL_TmPnt__update(curThreadId:ThreadId,tmPntVal:MG_TmPntV
 // 获取 本地函数   'clang-var运行时基础 中的 TL_TmPnt__update(tmPntVal)'
 function get_nativeFn__clgVarRt__TL_TmPnt__update():void{
   const fnAdr__clgVarRt__TL_TmPnt__update:NativePointer = DebugSymbol.fromName("TL_TmPnt__update").address;
-  g_nativeFn__clgVarRt__TL_TmPnt__update=  new NativeFunction(fnAdr__clgVarRt__TL_TmPnt__update, 'void',['int','int']);
+  mg_nativeFn__clgVarRt__TL_TmPnt__update=  new NativeFunction(fnAdr__clgVarRt__TL_TmPnt__update, 'void',['int','int']);
 }
