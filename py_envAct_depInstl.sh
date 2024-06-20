2aa6b5bb2ade40694e37f9f115d432f37fa13b60
prgrmz07 <prgrmz07@163.com>
2024-06-21 00:07:22 +0800

    请确认是 miniconda中的pip、python

--- py_envAct_depInstl.sh
+++ py_envAct_depInstl.sh
@@ -8,7 +8,12 @@ cd /fridaAnlzAp/frida_js/
 # set +x ; 
 source /app/Miniconda3-py310_22.11.1-1/bin/activate ; 
 # set -x
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple > /dev/null
+ #请确认是 miniconda中的pip
+[[ "$(sudo PATH=$PATH  which pip)" == "/app/Miniconda3-py310_22.11.1-1/bin/pip" ]] || exit 11
+ #请确认是 miniconda中的python
+[[ "$(sudo PATH=$PATH  which python)" == "/app/Miniconda3-py310_22.11.1-1/bin/python" ]] || exit 12
+sudo PATH=$PATH python -m pip install --upgrade pip #升级pip
+sudo PATH=$PATH pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple > /dev/null
 # py依赖安装
-pip install -r requirements.txt > /dev/null
+sudo PATH=$PATH pip install -r requirements.txt 
 
