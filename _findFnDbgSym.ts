b9ca427180a6e51edc101adfb3637940418c6353
prgrmz07 <prgrmz07@163.com>
2024-06-20 10:17:39 +0800

    自制拆分ts ： 拆出_findFnDbgSym.ts [1]

---
+++ _findFnDbgSym.ts
@@ -0,0 +1,25 @@
+//填充函数符号表格
+function findFnDbgSym(fnAdr:NativePointer,  _gFnSymTab:Map<FnAdrHex,DebugSymbol>):DebugSymbol {
+  // 相同内容的NativePointer可以是不同的对象，因为不能作为Map的key，必须用该NativePointer对应的字符串作为Map的key
+  const fnAdrHex:FnAdrHex=adrToHex(fnAdr);
+  let fnSym:DebugSymbol|undefined=_gFnSymTab.get(fnAdrHex);
+      if(fnSym!=null && fnSym!=undefined){ // !isNil(fnSym)
+        // console.log(`##从缓存获得调试信息，${fnAdr}`);
+        return fnSym;
+      }
+
+        //函数地址k的详情
+        fnSym=DebugSymbol.fromAddress(fnAdr);
+
+        // const modNm:string|null=fnSym.moduleName;
+        // const fileNm:string|null=fnSym.fileName;
+
+        //打印函数地址k
+        console.log(`##只有首次查调试信息文件，${JSON.stringify(fnSym)}`);
+
+        //该函数地址插入表格: 建立 函数地址 到 函数调试符号详情 的 表格
+        _gFnSymTab.set(fnAdrHex, fnSym);
+
+        return fnSym
+
+}
