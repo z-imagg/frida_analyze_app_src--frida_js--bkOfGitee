f15b8567299ef171f1db0e7a0dcd7a1b530b16e6
prgrmz07 <prgrmz07@163.com>
2024-06-20 19:26:08 +0800

    应用运行前准备工作

---
+++ pre_appRun.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#[描述] 应用运行前准备工作
+#[使用] bash /fridaAnlzAp/frida_js/pre_appRun.sh
+#[依赖] https://prgrmz07.coding.net/p/app/d/sleuthkit/git/tree/brch%2Fsleuthkit-4.12.1/disk_partition_example/readme.md 
+
+function pre_appRun(){
+
+local img_Hm=/app2/sleuthkit/disk_partition_example
+local part_ddImg_F=${img_Hm}/256M_FAT32.dd.bs4M
+local part_ddImg_comparess_F=${part_ddImg_F}.tar.gz
+
+local Err2=2
+local Err2Msg="[错误] 磁盘分区dd镜像压缩文件不存在, 错误代码[$Err2]"
+[[ -f $part_ddImg_comparess_F ]] || {  echo $Err2Msg ; return $Err2 ;}
+
+tar -xzvf $part_ddImg_comparess_F -C $img_Hm
+
+local Err3=3
+local Err3Msg="[错误] 磁盘分区dd镜像文件不存在, 错误代码[$Err3]"
+[[ -f $part_ddImg_F ]] || {  echo $Err3Msg ; return $Err3 ;}
+
+local OkMsg="[成功] 获得磁盘分区dd镜像文件 [$part_ddImg_F]:"
+echo $OkMsg && ls -lh $part_ddImg_F
+}
+
+
+pre_appRun
