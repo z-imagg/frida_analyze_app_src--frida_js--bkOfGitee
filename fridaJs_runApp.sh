4ef8f638a4ccfb483735f47b2a8863727e957cc4
prgrmz07 <prgrmz07@163.com>
2024-06-20 16:53:11 +0800

    日志后处理(日志文件更名)

--- fridaJs_runApp.sh
+++ fridaJs_runApp.sh
@@ -34,26 +34,8 @@ sudo env "PATH=$PATH" frida  --load $outJsFPath        --file  $_appPath
 ls -lht $logFPattern
 
 exit 0
-#日志后处理(暂时不要)
-now="$(date +%s)"
+#日志后处理(日志文件更名)
 #  'appName--' 是analyze_by_graph/config.py获取应用名称的依据, 不要乱动 
-FridaOut="/gain/frida-out/appName--qemu-system-x86_64--v8.2.2" && mkdir -p ${FridaOut}
-_LogFp_App="appOut-${now}.log"
-_LogFP_Mix="${FridaOut}/Mix-${now}.log"
-_LogFP_PrefPure="${FridaOut}/PrefixPure-${now}.log"
+FridaOut="/gain/frida-out/appName--${_appName}" && mkdir -p ${FridaOut}
 _LogFP_Pure="${FridaOut}/Pure-${now}.log"
-#  目前日志文件软链接
-_LogFP_PureNow_link="/gain/frida-out/PureNow.log"
-md5sum $_LogFP_Mix > $_LogFP_Mix.md5sum.txt
-# 日志后处理
-#   提取出带前缀的纯净日志， 并 记录日志文件的数字签名
-grep __@__@   $_LogFP_Mix >  $_LogFP_PrefPure
-md5sum $_LogFP_PrefPure > $_LogFP_PrefPure.md5sum.txt
-#   去掉前缀成为纯净日志， 并 记录日志文件的数字签名
-sed 's/^__@__@//' $_LogFP_PrefPure > $_LogFP_Pure
-#重新创建目前日志文件软链接"/gain/frida-out/PureNow.log"
-unlink $_LogFP_PureNow_link ; ln -s $_LogFP_Pure $_LogFP_PureNow_link
-md5sum $_LogFP_Pure > $_LogFP_Pure.md5sum.txt
-
-#最终产物日志文件名举例： frida-out-Pure-1712031317.log  
-#    其数字签名举例： frida-out-Pure-1712031317.log.md5sum.txt
+mv $(ls  $logFPattern) $_LogFP_Pure
