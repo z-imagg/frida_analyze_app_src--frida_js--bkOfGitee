54ed77c6afbf3e4a2addd5cd4e376b71f8f3fd04
prgrmz07 <prgrmz07@163.com>
2024-06-20 12:00:00 +0800

    自制拆分ts ： 拆出_focus_fnAdr [1]

--- InterceptFnSym.ts
+++ InterceptFnSym.ts
@@ -74,104 +74,8 @@ function OnFnLeaveBusz(thiz:InvocationContext,  retval:any ){
   console.log(`${LogLinePrefix}${fnLeaveLog.toJson()}`)
 }
 
-//是否关注该函数
-function focus_fnAdr(fnAdr:NativePointer, _g_appName:string){
-  const fnSym=DebugSymbol.fromAddress(fnAdr);
-  const moduleName = fnSym.moduleName
-  if(moduleName==null){
-    throw new Error(`【断言失败】moduleName为null`)
-  }
-
-  //不关注名为空的函数
-  if (fnSym.name==null || fnSym.name==undefined){
-    console.log(`##不关注名为空的函数.fnAdr=[${fnAdr}]`)
-    return false;
-  }
-
-// 解决frida拦截目标进程中途崩溃 步骤  == frida_js_skip_crashFunc_when_Interceptor.attach.onEnter.md 
-
-// 日志量高达3千万行。 疑似特别长的有 pit_irq_timer 、 generate_memory_topology ， 尝试跳过
-  // 模块g_appName 中通常有函数被关注
-  if(moduleName==_g_appName   ){
-    // 'if ... return' 只关注给定条件, 不需要 全局条件 'return ...'   
-    if  (
-      //跳过:
-      [
-//跳过clang-var的c运行时 runtime_c__vars_fn
-      "_init_varLs_inFn__RtC00", "createVar__RtC00", "destroyVarLs_inFn__RtC00",
-//跳过clang-var的c++运行时 runtime_cpp__vars_fn
-      // "_init_varLs_inFn__RtCxx", "createVar__RtCxx", "destroyVarLs_inFn__RtCxx", 
-      //执行命令  objdump --syms  /server_root/fridaAnlzAp/clang-var/build/runtime_cpp__vars_fn/libclangPlgVar_runtime_cxx.a
-      //发现 这些原始c++函数名 对应的abi函数名如下
-      "_Z23_init_varLs_inFn__RtCxxNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEES4_ii", "_Z16createVar__RtCxxP11__VarDeclLsNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEEi", "_Z24destroyVarLs_inFn__RtCxxP11__VarDeclLs",
-//跳过clang-var的运行时基础 runtime_c__TmPnt_ThreadLocal
-      "TL_TmPnt__update", "TL_TmPnt__get", "TL_TmPnt__printPtr",
-    ].includes(fnSym.name) || 
-//跳过qemu的巨量调用函数们:
-//  frida_js运行qemu, ..., 直到 analyze_by_graph,  analyze_by_graph能提供调用次数
-      fnSym.name == "pit_irq_timer" ||
-      fnSym.name == "generate_memory_topology"||
-      fnSym.name == "ffi_call" ||
-//analyze_by_graph 打印大于1万次调用的函数们（方便返工修改frida_js以跳过大量调用函数）
-      ["symcmp64",
-      "pic_get_irq",
-      "pic_update_irq",
-      "pic_stat_update_irq",
-      "pic_set_irq",
-      "apic_accept_pic_intr",
-      "pic_irq_request",
-      "gsi_handler",
-      "ioapic_set_irq",
-      "icount_notify_exit",
-      "ioapic_stat_update_irq",
-      "qemu_timer_notify_cb",
-      "pit_get_next_transition_time",
-      "hpet_handle_legacy_irq",
-      "pit_get_out",
-      "pit_irq_timer_update.part.0",
-
-      "victim_tlb_hit",
-      "mmu_lookup",		
-      "mmu_lookup1",	
-      "helper_stb_mmu",
-      "helper_ldub_mmu",
-      
-    ].includes(fnSym.name) || 
-      false
-    )  {
-      return false;
-    }
-  }
-
-  if(moduleName=="libffi.so.8"){
-    // 'if ... return' 只关注给定条件, 不需要 全局条件 'return ...'   
-    if (
-      //跳过:
-      fnSym.name == "ffi_call"
-    ){
-      return false;
-    }
-  }
-
-/**已确认 结束时frida出现'Process terminated' 对应的进程qphotorec有正常退出码0
-https://gitee.com/repok/dwmkerr--linux-kernel-module/blob/e36a16925cd60c6e4b3487d254bfe7fa5b150f75/greeter/run.sh
-*/
-  //除上述特定关注外:
-  
-  //关注包含模块的所有函数
-  if(modules_include.includes(moduleName)){
-    //  全局条件 'return ...'   , 不需要 'if ... return' 只关注给定条件
-    return true;
-  }
-  //忽略排除模块的所有函数
-  if(modules_exclude.includes(moduleName)){
-    //  全局条件 'return ...'   , 不需要 'if ... return' 只关注给定条件
-    return false;
-  }
-
-  //其他情况 跳过
-  return false;
-}
+// 导入 ' _focus_fnAdr.ts 是否关注该函数 '
+//MyTsCmd//_replaceCurLineByTsFileContent("./_focus_fnAdr.ts" , curNextLn)
 
 function _main_(){
   // 获取 本地函数   'clang-var运行时基础 中的 TL_TmPnt__update(tmPntVal)'
@@ -212,51 +116,6 @@ const g_appFullPath: string ='/app/qemu/build-v8.2.2/qemu-system-x86_64';
 //应用程序名字
 const g_appName: string =baseNameOfFilePath(g_appFullPath);
 
-/**
-ldd /app/qemu/build-v8.2.2/qemu-system-x86_64
-        linux-vdso.so.1 (0x00007ffff7fc1000)
-        libpixman-1.so.0 => /lib/x86_64-linux-gnu/libpixman-1.so.0 (0x00007ffff67a2000)
-        libz.so.1 => /lib/x86_64-linux-gnu/libz.so.1 (0x00007ffff6786000)
-        libgio-2.0.so.0 => /lib/x86_64-linux-gnu/libgio-2.0.so.0 (0x00007ffff65ad000)
-        libgobject-2.0.so.0 => /lib/x86_64-linux-gnu/libgobject-2.0.so.0 (0x00007ffff654d000)
-        libglib-2.0.so.0 => /lib/x86_64-linux-gnu/libglib-2.0.so.0 (0x00007ffff6413000)
-        libgmodule-2.0.so.0 => /lib/x86_64-linux-gnu/libgmodule-2.0.so.0 (0x00007ffff640a000)
-        libm.so.6 => /lib/x86_64-linux-gnu/libm.so.6 (0x00007ffff6323000)
-        libc.so.6 => /lib/x86_64-linux-gnu/libc.so.6 (0x00007ffff60fa000)
-        /lib64/ld-linux-x86-64.so.2 (0x00007ffff7fc3000)
-        libmount.so.1 => /lib/x86_64-linux-gnu/libmount.so.1 (0x00007ffff60b6000)
-        libselinux.so.1 => /lib/x86_64-linux-gnu/libselinux.so.1 (0x00007ffff608a000)
-        libffi.so.8 => /lib/x86_64-linux-gnu/libffi.so.8 (0x00007ffff607d000)
-        libpcre.so.3 => /lib/x86_64-linux-gnu/libpcre.so.3 (0x00007ffff6005000)
-        libblkid.so.1 => /lib/x86_64-linux-gnu/libblkid.so.1 (0x00007ffff5fce000)
-        libpcre2-8.so.0 => /lib/x86_64-linux-gnu/libpcre2-8.so.0 (0x00007ffff5f37000)
-*/
-
-//关注模块
-const modules_include=[
-  "other_module_1.so",
-];
-// "libstdc++.so.6.0.30", //?如果libstdc++的代码 穿插在业务代码中， 若忽略之 则调用链条断裂
-// ldd /app/qemu/build-v8.2.2/qemu-system-x86_64 | awk '{print " \""$1"\","}'
-//排除模块
-const modules_exclude:string[]=[
- "linux-vdso.so.1",
- "libpixman-1.so.0",
- "libz.so.1",
- "libgio-2.0.so.0",
- "libgobject-2.0.so.0",
- "libglib-2.0.so.0",
- "libgmodule-2.0.so.0",
- "libm.so.6",
- "libc.so.6",
- "/lib64/ld-linux-x86-64.so.2",
- "libmount.so.1",
- "libselinux.so.1",
- "libffi.so.8",//被qemu大量调用的ffi_call在此模块libffi.so中
- "libpcre.so.3",
- "libblkid.so.1",
- "libpcre2-8.so.0",
-];
 
 /**
 frida 运行报超时错误 "Failed to load script: timeout was reached" 解决
