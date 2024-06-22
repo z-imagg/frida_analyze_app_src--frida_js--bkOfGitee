d75fe3d3d4af1bdda5c8bbeca8da4418544c1132
prgrmz07 <prgrmz07@163.com>
2024-06-22 15:08:57 +0800

    整理，剔除干净demo中的名字

--- _Fn05OutArg.ts
+++ _Fn05OutArg.ts
@@ -3,10 +3,7 @@
 enum Fn05ArgIdx{
 // destroyVarLs_inFn__RtC00 函数签名
 // /fridaAnlzAp/clang-var/runtime_c__vars_fn/include/runtime_c__vars_fn.h
-// void destroyVarLs_inFn__RtC00(_VarDeclLs *_vdLs, int jTxtOLimit, char* jsonTxtOut_, int* jTxtOLenOut_);
 // void destroyVarLs_inFn__RtC00(_VarDeclLs * _vdLs, int jTxtOLimit, char* jsonTxtOut_, int* jTxtOLenOut_);
-// float func05_userQuery(char sex, int userId, int userName_limit, char* userName_out_, int* userName_length_out_);
-
     _vdLs=0,
     jTxtOLimit=1,
     jsonTxtOut_=2,
@@ -36,21 +33,19 @@ class FnOutArg_DestroyRtC00{
   ){
     
     
-    // const arg0_toInt32:number=args[0].toInt32() // ==  sex
+    // const arg0_toInt32:number=args[0].toInt32() // ==  _vdLs
     
-    // const arg1_toInt32:number=args[1].toInt32() // == userId
-    
-    // args[2].toInt32() // == userName_limit
-    args[Fn05ArgIdx.jTxtOLimit]=new NativePointer(_int__jTxtOLimit);// 修改 输入参数 userName_limit 为 _int__jTxtOLimit
+    // args[1].toInt32() // == jTxtOLimit
+    args[Fn05ArgIdx.jTxtOLimit]=new NativePointer(_int__jTxtOLimit);// 修改 输入参数 jTxtOLimit 为 _int__jTxtOLimit
     logWriteLn(`[frida_js Fn05OutArg.constructor] jTxtOLimit=[${_int__jTxtOLimit}]`); 
     this.int__jTxtOLimit=_int__jTxtOLimit
     
-    // const arg3_readCString:string| null=args[3].readCString() // == jsonTxtOut_
+    // args[2].readCString() // == jsonTxtOut_
     args[Fn05ArgIdx.jsonTxtOut_]=_charArr__jsonTxtOut_ // 修改 入参 jsonTxtOut_ 为 _charArr__jsonTxtOut_
     logWriteLn(`[frida_js Fn05OutArg.constructor] jsonTxtOut_=[${_charArr__jsonTxtOut_}]`); 
     this.charArr__jsonTxtOut_=_charArr__jsonTxtOut_ //保留 之
     
-    // const arg4_readInt:number=args[4].readInt() // == jTxtOLenOut_
+    //  args[3].readInt() // == jTxtOLenOut_
     args[Fn05ArgIdx.jTxtOLenOut_]=_intPtr_jTxtOLenOut_ // 修改 入参 jTxtOLenOut_ 为 _intPtr_jTxtOLenOut_
     logWriteLn(`[frida_js Fn05OutArg.constructor] jTxtOLenOut_=[${_intPtr_jTxtOLenOut_}]`); 
     this.intPtr_jTxtOLenOut_=_intPtr_jTxtOLenOut_ //保留 之
@@ -59,28 +54,24 @@ class FnOutArg_DestroyRtC00{
     //离开函数func05_userQuery的处理
       Leave(){
       //现在是函数离开时, 由于函数进入时 参数们args[k]被保存在thiz下, 因此此时可以拿出来
-      // const userName_limit:NativePointer=this.int__userName_limit
-      // const userName_out_:NativePointer=this.charArr__jsonTxtOut_
-      // const userName_length_out_:NativePointer=this.intPtr_userName_length_out_
       logWriteLn(`[frida_js Fn05OutArg.Leave] json(this)=[${JSON.stringify(this)}]`);
     
-      // func05_userQuery 函数签名
-      // /fridaAnlzAp/frida_js_demo/app.c
-      // float func05_userQuery(char sex, int userId, int userName_limit, char* userName_out_, int* userName_length_out_);
+// destroyVarLs_inFn__RtC00 函数签名
+// void destroyVarLs_inFn__RtC00(_VarDeclLs * _vdLs, int jTxtOLimit, char* jsonTxtOut_, int* jTxtOLenOut_);
       
-      const arg2_toInt32:number=this.int__jTxtOLimit // == userName_limit
-      logWriteLn(`[frida_js  Fn05OutArg.Leave] arg2_toInt32=[${arg2_toInt32}]`); 
+      //this.int__jTxtOLimit // == jTxtOLimit
+      logWriteLn(`[frida_js  Fn05OutArg.Leave] int__jTxtOLimit=[${this.int__jTxtOLimit}]`); 
     
-      //函数离开时, 获取到 函数出参 userName_out_
-      const arg3_readCString:string| null=this.charArr__jsonTxtOut_.readCString() // == userName_out_
+      //函数离开时, 获取到 函数出参 jsonTxtOut_
+      const arg3_readCString:string| null=this.charArr__jsonTxtOut_.readCString() // == jsonTxtOut_
       if(arg3_readCString){
         logWriteLn(`[frida_js  Fn05OutArg.Leave] arg3_readCString=[${arg3_readCString}]`);
       }
       
-      //函数离开时, 获取到 函数出参 userName_length_out_
-      const arg4_readInt:number=this.intPtr_jTxtOLenOut_.readInt() // == userName_length_out_
+      //函数离开时, 获取到 函数出参 jTxtOLenOut_
+      const arg4_readInt:number=this.intPtr_jTxtOLenOut_.readInt() // == jTxtOLenOut_
       logWriteLn(`[frida_js  Fn05OutArg.Leave] arg4_readInt=[${arg4_readInt}]`);
-      // this.intPtr_jTxtOLenOut_.writeInt(-88); // 修改 输入参数 userName_length_out_ 为 -88; 这句是为了实验，并无业务目的
+      // this.intPtr_jTxtOLenOut_.writeInt(-88); // 修改 输入参数 jTxtOLenOut_ 为 -88; 这句是为了实验，并无业务目的
       }
     
     }
