e636d9b0873d16f4c4993d35d6fde58626db8347
prgrmz07 <prgrmz07@163.com>
2024-06-23 22:20:40 +0800

    根据这里的业务，做出相应修改

--- _CxxFnOutArg_stdString__Fn06.ts
+++ _CxxFnOutArg_stdString__Fn06.ts
@@ -1,20 +1,20 @@
 // [依赖] : _logFile.ts/logWriteLn
-// [描述] : clang-var插件中runtime c00中destroy函数json串出参 操纵
+// [描述] : clang-var插件中runtime cxx中destroy函数json串出参 操纵
 
-// const mg_abiName__cxxFunc06_outArgString:string="_Z22cxxFunc06_outArgStringiPNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE";
-const mg_fnName__cxxFunc06_outArgString:string="cxxFunc06_outArgString";
+// const mg_abiName__cxxFunc06_outArgString:string="_Z24destroyVarLs_inFn__RtCxxP11__VarDeclLsPNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE";
+const mg_fnName__DestroyRtCxx_outArgString:string="destroyVarLs_inFn__RtCxx";
 
 enum FnArgIdx_Fn06{
 // cxxFunc06_outArgString 函数签名
-// /fridaAnlzAp/frida_js_demo/app.cpp
-// bool cxxFunc06_outArgString(int num,  std::string * numDescOut_);
-    num=0,
-    numDescOut_=1,
+//  /fridaAnlzAp/clang-var/runtime_cpp__vars_fn/include/runtime_cpp__vars_fn.h
+// void destroyVarLs_inFn__RtCxx(_VarDeclLs * _vdLs, std::string * jsonTxtOut_)
+    _vdLs=0,
+    jsonTxtOut_=1,
     // arg2Name=2, //这一行是举例而已，此函数只有以上两个参数
 }
 
-//clang-var插件中runtime c00中destroy函数json串出参 操纵.
-class CxxFnOutArg_stdString__Fn06{
+//clang-var插件中runtime cxx中destroy函数json串出参 操纵.
+class CxxFnOutArg_DestroyRtCxx{
 
   //  /fridaAnlzAp/frida_js_demo/app.cpp / fridaHelper__cxxFuncWrap__std_string_cstr / _Err1 == 1
   static  stdStr_2_fridaBuf__Err1:number = 1;
@@ -23,24 +23,22 @@ class CxxFnOutArg_stdString__Fn06{
   //  /fridaAnlzAp/frida_js_demo/app.cpp / fridaHelper__cxxFuncWrap__std_string_cstr / _stdStr_2_fridaBuf_gap == 11
   static   stdStr_2_fridaBuf_gap:number = 11;
 
-  int__num:number
-  ptrCxxStdStr__numDescOut_:NativePointer
+  ptrCxxStdStr__jsonTxtOut_:NativePointer
     
   static Enter(
     args:InvocationArguments,
-    _int__num:number,
-  ):CxxFnOutArg_stdString__Fn06|null{
+  ):CxxFnOutArg_DestroyRtCxx|null{
 
-  let  _ptrCxxStdStr__numDescOut_:NativePointer;
+  let  _ptrCxxStdStr__jsonTxtOut_:NativePointer;
     //调用本地函数 fridaHelper__cxxFuncWrap__std_string_new 
-    _ptrCxxStdStr__numDescOut_=nativeFn__fridaHelper__cxxFuncWrap__std_string_new( ) ;
+    _ptrCxxStdStr__jsonTxtOut_=nativeFn__fridaHelper__cxxFuncWrap__std_string_new( ) ;
     //等效c++语句为
-    //std::string* _ptrCxxStdStr__numDescOut_=new std::string()
+    //std::string* _ptrCxxStdStr__jsonTxtOut_=new std::string()
 
-    logWriteLn(`[frida_js Fn05OutArg.Enter] _ptrCxxStdStr__numDescOut_=[${_ptrCxxStdStr__numDescOut_}]`); 
+    logWriteLn(`[frida_js CxxFnOutArg_DestroyRtCxx.Enter] _ptrCxxStdStr__jsonTxtOut_=[${_ptrCxxStdStr__jsonTxtOut_}]`); 
     
-    if(_ptrCxxStdStr__numDescOut_){
-      return new CxxFnOutArg_stdString__Fn06(args, _int__num  ,_ptrCxxStdStr__numDescOut_);
+    if(_ptrCxxStdStr__jsonTxtOut_){
+      return new CxxFnOutArg_DestroyRtCxx(args,  _ptrCxxStdStr__jsonTxtOut_);
     }
 
   return null;
@@ -50,49 +48,45 @@ class CxxFnOutArg_stdString__Fn06{
 //给出参赋以全局内存空间
   constructor(
     args:InvocationArguments,
-    _int__num:number,
-    _ptrCxxStdStr__numDescOut_:NativePointer
+    _ptrCxxStdStr__jsonTxtOut_:NativePointer
   ){
 
-    // args[1].toInt32() // == num
-    args[FnArgIdx_Fn06.num]=new NativePointer(_int__num);// 修改 输入参数 num 为 _int__num
-    this.int__num=_int__num
-    logWriteLn(`[frida_js Fn05OutArg.constructor] num=[${_int__num}]`); 
+    // 不关注参数 _vdLs
+    // args[0].toInt32() // =?= 指针_vdLs的值
     
-    // args[2].readCString() // == numDescOut_
-    args[FnArgIdx_Fn06.numDescOut_]=_ptrCxxStdStr__numDescOut_ // 修改此次函数调用 出参 numDescOut_ 为 _ptrCxxStdStr__numDescOut_
-    this.ptrCxxStdStr__numDescOut_=_ptrCxxStdStr__numDescOut_ //保留 之
+    // args[1] //  jsonTxtOut_
+    args[FnArgIdx_Fn06.jsonTxtOut_]=_ptrCxxStdStr__jsonTxtOut_ // 修改此次函数调用 出参 jsonTxtOut_ 为 _ptrCxxStdStr__jsonTxtOut_
+    this.ptrCxxStdStr__jsonTxtOut_=_ptrCxxStdStr__jsonTxtOut_ //保留 之
     
     }
   
   //拿出参内容
   Leave(){
     //现在是函数离开时, 由于函数进入时 参数们args[k]被保存在thiz下, 因此此时可以拿出来
-    logWriteLn(`[frida_js Fn05OutArg.Leave] json(this)=[${JSON.stringify(this)}]`);
+    logWriteLn(`[frida_js CxxFnOutArg_DestroyRtCxx.Leave] json(this)=[${JSON.stringify(this)}]`);
 
 // cxxFunc06_outArgString 函数签名
-// /fridaAnlzAp/frida_js_demo/app.cpp
-// bool cxxFunc06_outArgString(int num,  std::string * numDescOut_);
+//  /fridaAnlzAp/clang-var/runtime_cpp__vars_fn/include/runtime_cpp__vars_fn.h
+// void destroyVarLs_inFn__RtCxx(_VarDeclLs * _vdLs, std::string * jsonTxtOut_)
     
-    //this.int__num // == num
-    logWriteLn(`[frida_js  Fn05OutArg.Leave] int__num=[${this.int__num}]`); 
-
-    //函数离开时, 获取到 函数出参 numDescOut_
-    if(this.ptrCxxStdStr__numDescOut_){
+    // 不关注参数 _vdLs
+    
+    //函数离开时, 获取到 函数出参 jsonTxtOut_
+    if(this.ptrCxxStdStr__jsonTxtOut_){
         //调用std::string::size方法
-        const stdStr_size:number=nativeFn__fridaHelper__cxxFuncWrap__std_string_size(this.ptrCxxStdStr__numDescOut_)
+        const stdStr_size:number=nativeFn__fridaHelper__cxxFuncWrap__std_string_size(this.ptrCxxStdStr__jsonTxtOut_)
         //为cstr分配缓冲区
-        const cStrBuf:NativePointer=Memory.alloc(stdStr_size+CxxFnOutArg_stdString__Fn06.stdStr_2_fridaBuf_gap+1);
+        const cStrBuf:NativePointer=Memory.alloc(stdStr_size+CxxFnOutArg_DestroyRtCxx.stdStr_2_fridaBuf_gap+1);
         //调用std::string::c_str方法
-        const retCode:number=nativeFn__fridaHelper__cxxFuncWrap__std_string_cstr(this.ptrCxxStdStr__numDescOut_, stdStr_size, cStrBuf);
-        if(retCode==CxxFnOutArg_stdString__Fn06.stdStr_2_fridaBuf__OK){
-          const numDescOut_CStr:string| null=cStrBuf.readCString() // == numDescOut_
-          if(numDescOut_CStr){
-            logWriteLn(`[frida_js  CxxFnOutArg_stdString__Fn06.Leave] numDescOut_CStr=[${numDescOut_CStr}]`);
+        const retCode:number=nativeFn__fridaHelper__cxxFuncWrap__std_string_cstr(this.ptrCxxStdStr__jsonTxtOut_, stdStr_size, cStrBuf);
+        if(retCode==CxxFnOutArg_DestroyRtCxx.stdStr_2_fridaBuf__OK){
+          const jsonTxtOut_CStr:string| null=cStrBuf.readCString() // == jsonTxtOut_
+          if(jsonTxtOut_CStr){
+            logWriteLn(`[frida_js  CxxFnOutArg_DestroyRtCxx.Leave] jsonTxtOut_CStr=[${jsonTxtOut_CStr}]`);
           }
         }
-        //等效 c++语句为 delete ptrCxxStdStr__numDescOut_
-        nativeFn__fridaHelper__cxxFuncWrap__std_string_delete(this.ptrCxxStdStr__numDescOut_ ) ;
+        //等效 c++语句为 delete ptrCxxStdStr__jsonTxtOut_
+        nativeFn__fridaHelper__cxxFuncWrap__std_string_delete(this.ptrCxxStdStr__jsonTxtOut_ ) ;
     }
     
     }
